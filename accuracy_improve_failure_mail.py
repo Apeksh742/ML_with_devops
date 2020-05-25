01ce4b12bbcba2933b2555b9919b5aa44bc1b11c
Apeksh Agarwal <59861221+Apeksh742@users.noreply.github.com>
2020-05-25 11:07:35 +0530

    Update accuracy_improve_failure_mail.py

--- accuracy_improve_failure_mail.py
+++ accuracy_improve_failure_mail.py
@@ -6,16 +6,21 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls() 
   
-# Authentication 
-s.login("Apekshagarwal369@gmail.com", "rj14sz7820") 
+# Authentication
+sender_email = "Apekshagarwal369@gmail.com"
+sender_password = "rj14sz7820"
+receiver_email = "Apekshagarwal8@gmail.com"
+s.login(sender_email, sender_password )
   
 # message to be sent 
-message = '''Hello, 
-				Developer this is an email regarding your last commit. It seems that your accuracy_improve.py is not working properly please check it once and recommit.
+message = '''/
+Subject: Build improve_accuracy failed
+Hello, 
+       Developer this is an email regarding your last commit. It seems that your accuracy_improve.py is not working properly please check it once and recommit.
 			THANK YOU'''
   
 # sending the mail 
-s.sendmail("Apekshagarwal369@gmail.com", "Apekshagarwal8@gmail.com", message) 
+s.sendmail(sender_email,receiver_email , message)
   
 # terminating the session 
 s.quit() 
